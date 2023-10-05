30ddc8ae8b577c150205fd2f78fa2919ea6ab827
student <jan.kowalski@pollub.edu.pl>
2023-10-05 14:38:24 +0200

    1 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,5 +15,11 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+
+        public void hello (String Object cos;
+        cos) {
+            System.out.println();
+        }
+
     }
 }
